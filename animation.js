cc058b940bbdc33e26cb915363f4ca7450bb6724
Rathin Kumar Gorai <89589036+Rathin-Gorai@users.noreply.github.com>
2022-02-10 19:36:40 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,11 @@
+window.onload =()=>{
+                confetti.start()
+                Swal.fire({
+                    title:"Happy Birthday Vaishanavi di",
+                    text:`I didnt know that today is your birthday I would have created something better
+                    Card created by Birthday Card Maker by me.`,
+                    button:"Lets Go"
+                })
+                
+                document.getElementById("HBDmp").play()
+                }
