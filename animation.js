a6aff2290db7d9f158bdf8a067840435cb0ac912
Rathin Kumar Gorai <rthnkg@gmail.com>
2023-02-16 07:43:40 +0530

    maam

--- animation.js
+++ animation.js
@@ -1,7 +1,7 @@
 window.onload =()=>{
                 confetti.start()
                 Swal.fire({
-                    title:"🎂 Happy Birthday Neha 🎂",
+                    title:"🎂 Happy Birthday Ma'am 🎂",
                     text:`Happy birthday! I hope all your birthday wishes and dreams come true.`,
                     button:"Lets Go" 
                 }).then((result) => {
